3d2c3a11fdea95e0d7837d069024d97d191f5492
Gabriel <gabrieldantaslp@gmail.com>
2024-09-21 15:50:17 -0300

    refactor: Updating custom exceptions on userService

--- src/main/java/com/gabriel/ecommerce/services/UserService.java
+++ src/main/java/com/gabriel/ecommerce/services/UserService.java
@@ -4,56 +4,55 @@ import java.util.List;
 import java.util.Optional;
 
 
+import com.gabriel.ecommerce.services.exceptions.DataIntegrityViolationException;
+import com.gabriel.ecommerce.services.exceptions.ObjectNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.gabriel.ecommerce.entities.User;
 import com.gabriel.ecommerce.repositories.UserRepository;
-import com.gabriel.ecommerce.services.exceptions.DatabaseException;
-import com.gabriel.ecommerce.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException(id));
+		return obj.orElseThrow(() -> new ObjectNotFoundException("The User with id: " + id + " not found!"));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		try {
 			repository.deleteById(id);
 		} catch (EmptyResultDataAccessException e) {
-			throw new ObjectNotFoundException(id);
+			throw new ObjectNotFoundException("The User with id: " + id + " not found!");
 		} catch (DataIntegrityViolationException e) {
-			throw new DatabaseException(e.getMessage());
+			throw new DataIntegrityViolationException(e.getMessage());
 		}
-		
+
 	}
-	
+
 	public User update(Long id, User obj) {
 		try {
 			User entity = repository.getOne(id);
 			updateData(entity, obj);
 			return repository.save(entity);
 		} catch (EntityNotFoundException e) {
-			throw new ObjectNotFoundException(id);
+			throw new ObjectNotFoundException("The User with id: " + id + " not found!");
 		}
-		
+
 	}
 
 	private void updateData(User entity, User obj) {
