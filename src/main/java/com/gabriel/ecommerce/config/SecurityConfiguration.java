1c215ba5bb08354363f7f0cf20732be7cb86f28a
Gabriel <gabrieldantaslp@gmail.com>
2024-09-21 14:06:07 -0300

    fix: Returning the h2 console access and encoding passwords for mocked users

--- src/main/java/com/gabriel/ecommerce/config/SecurityConfiguration.java
+++ src/main/java/com/gabriel/ecommerce/config/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -31,6 +32,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .authorizeHttpRequests(req ->
                         req.requestMatchers(WHITE_LIST_URL)
                                 .permitAll()
--- src/main/java/com/gabriel/ecommerce/config/TestConfig.java
+++ src/main/java/com/gabriel/ecommerce/config/TestConfig.java
@@ -7,6 +7,7 @@ import com.gabriel.ecommerce.auth.AuthenticationService;
 import com.gabriel.ecommerce.auth.RegisterRequest;
 import com.gabriel.ecommerce.entities.*;
 import com.gabriel.ecommerce.entities.enums.OrderStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -17,31 +18,23 @@ import com.gabriel.ecommerce.repositories.OrderItemRepository;
 import com.gabriel.ecommerce.repositories.OrderRepository;
 import com.gabriel.ecommerce.repositories.ProductRepository;
 import com.gabriel.ecommerce.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static com.gabriel.ecommerce.entities.enums.Role.ADMIN;
 import static com.gabriel.ecommerce.entities.enums.Role.MANAGER;
 
 @Configuration
 @Profile("test")
+@RequiredArgsConstructor
 public class TestConfig implements CommandLineRunner {
 
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private OrderRepository orderRepository;
-
-    @Autowired
-    private CategoryRepository categoryRepository;
-
-    @Autowired
-    private ProductRepository productRepository;
-
-    @Autowired
-    private OrderItemRepository orderItemRepository;
-
-    @Autowired
-    private AuthenticationService authenticationService;
+    private final UserRepository userRepository;
+    private final OrderRepository orderRepository;
+    private final CategoryRepository categoryRepository;
+    private final ProductRepository productRepository;
+    private final OrderItemRepository orderItemRepository;
+    private final AuthenticationService authenticationService;
+    private final PasswordEncoder encoder;
 
     @Override
     public void run(String... args) throws Exception {
@@ -57,6 +50,7 @@ public class TestConfig implements CommandLineRunner {
         Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
         Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
         Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
         // Salvando
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
@@ -68,11 +62,11 @@ public class TestConfig implements CommandLineRunner {
         p3.getCategories().add(cat3);
         p4.getCategories().add(cat3);
         p5.getCategories().add(cat2);
+
         // Salvando os produtos categorizados no banco
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
 //		 Instanciando novos usuários
-
         var admin = RegisterRequest.builder()
                 .name("admin")
                 .email("admin@gmail.com")
@@ -91,8 +85,8 @@ public class TestConfig implements CommandLineRunner {
                 .build();
         System.out.println("Manager token: " + authenticationService.register(manager).getAccessToken());
 
-        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", encoder.encode("123456"));
+        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", encoder.encode("654321"));
 
 //		 Instanciando novos pedidos
         Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
@@ -108,6 +102,7 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
         OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
 //        Salvando items de pedidos no banco
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
