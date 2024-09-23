12920abd583761b6defa525f58084ce53d991042
Gabriel <gabrieldantaslp@gmail.com>
2024-09-23 13:01:42 -0300

    fix: Relocating requests to components

--- client/src/app/components/products-list/products-list.component.ts
+++ client/src/app/components/products-list/products-list.component.ts
@@ -3,20 +3,23 @@ import { Product } from "../../types/product.dto";
 import { AsyncPipe, NgFor, NgIf } from "@angular/common";
 import { CurrencyReaisPipe } from "../../pipes/currency-reais.pipe";
 import { Observable } from "rxjs";
+import { ProductService } from "../../services/product.service";
+import { MatProgressSpinner } from "@angular/material/progress-spinner";
 
 @Component({
   selector: "app-products-list",
   standalone: true,
-  imports: [NgFor, CurrencyReaisPipe, NgIf],
+  imports: [NgFor, CurrencyReaisPipe, NgIf, MatProgressSpinner, AsyncPipe],
   templateUrl: "./products-list.component.html",
   styleUrl: "./products-list.component.css",
 })
 export class ProductsListComponent {
-  @Input() products: Product[] = null;
-  constructor() {
-    console.log("🚀 ~ ProductsListComponent ~ products:", this.products);
-  }
   @Output() productToCart = new EventEmitter<Product>();
+  products$: Observable<Product[]> = null;
+
+  constructor(private productService: ProductService) {
+    this.products$ = this.productService.findAll();
+  }
 
   addToCart = (product: Product) => {
     this.productToCart.emit(product);
--- client/src/app/components/shop-by-category/category-list/category-list.component.ts
+++ client/src/app/components/shop-by-category/category-list/category-list.component.ts
@@ -1,18 +1,24 @@
-import { NgClass, NgFor } from "@angular/common";
+import { AsyncPipe, NgClass, NgFor, NgIf } from "@angular/common";
 import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { Category } from "../../../types/category.dto";
+import { Observable } from "rxjs";
+import { CategoryService } from "../../../services/category.service";
 
 @Component({
   selector: "app-category-list",
   standalone: true,
-  imports: [NgFor, NgClass],
+  imports: [NgFor, NgClass, NgIf, AsyncPipe],
   templateUrl: "./category-list.component.html",
   styleUrl: "./category-list.component.css",
 })
 export class CategoryListComponent {
-  @Input() categories: Category[] = [];
   @Output("navigate") onNavigate = new EventEmitter<number>();
+  categories$: Observable<Category[]> = null;
   activeCategory: number = 0;
+
+  constructor(private categoryService: CategoryService) {
+    this.categories$ = this.categoryService.findAll();
+  }
 
   navigate = (categoryId: number) => {
     this.activeCategory = categoryId;
